0fbf37fd9854ffa8b3e69f86435afc9f51ee87ad
EAguirre <aguirreernesto25@gmail.com>
2023-10-24 19:42:03 -0300

    Link a Home en Landing

--- src/Vistas/Landing/Landing.jsx
+++ src/Vistas/Landing/Landing.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => {
     return (
-        <h1>Este es el inicio</h1>
+        <div>
+            <Link to="/home">Ir a Home</Link>
+            <h1>Este es el inicio</h1>
+        </div>
     )
 }
 
