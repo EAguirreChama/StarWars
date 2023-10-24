ed3277631b33916a1e3a4104e2cb536c58e1d72c
EAguirre <aguirreernesto25@gmail.com>
2023-10-24 11:20:13 -0300

    Empezando again

--- src/Vistas/index.js
+++
@@ -1,7 +0,0 @@
-import Planetas from "./Planetas/Planetas";
-import Landing from "./Landing/Landing";
-import Personajes from "./Personajes/Personajes";
-import Naves from "./Naves/Naves";
-import Home from "./Home/Home";
-
-export { Planetas, Landing, Personajes, Naves, Home}
