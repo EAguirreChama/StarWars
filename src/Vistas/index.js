7894ed88c3750a89cb628a75bf19982da172b861
EAguirre <aguirreernesto25@gmail.com>
2023-10-24 11:26:17 -0300

    Rutas y Vistas Nuevas

---
+++ src/Vistas/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home.jsx"
+import Landing from "./Landing/Landing.jsx"
+import Naves from "./Naves/Naves.jsx"
+import Personajes from "./Personajes/Personajes.jsx"
+import Planetas from "./Planetas/Planetas.jsx"
+
+export {Home, Landing, Naves, Personajes, Planetas}
