764924b81b34b027376f076db2135597ffebfd74
EAguirre <aguirreernesto25@gmail.com>
2023-10-25 10:43:49 -0300

    TOY

--- src/Redux/Personajes/getPersonajeId.js
+++ src/Redux/Personajes/getPersonajeId.js
@@ -5,7 +5,7 @@ export const GET_PERSONAJE_ID = "GET_PERSONAJE_ID"
 
 export const getPersonajesId = (id) => {
     return async function (dispatch) {
-        const apiData = await axios.get(`${BASE_URL}/people/${id}`)
+        const apiData = await axios.get(`${BASE_URL}people/${id}`)
         const personajeId = apiData.data;
             dispatch ({ type: GET_PERSONAJE_ID , payload: personajeId})
     }
