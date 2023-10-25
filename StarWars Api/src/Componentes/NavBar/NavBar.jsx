87e8e754178a6aa1aa7e38f04ca05a6390abf22a
EAguirre <aguirreernesto25@gmail.com>
2023-10-25 15:51:55 -0300

    Estilos NavBar

--- StarWars Api/src/Componentes/NavBar/NavBar.jsx
+++ StarWars Api/src/Componentes/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import style from "./NavBar.module.css"
 
 const NavBar = () => {
     const location = useLocation()
@@ -6,12 +7,15 @@ const NavBar = () => {
     if(location.pathname === "/") return null
 
     return (
-        <div>
-            <Link to="/">Volver a Landing</Link>
-            {location.pathname !== "/home"      && <Link to="/home">Ir a Home</Link>}
-            {location.pathname !== "/people"    && <Link to="/people">People</Link>}
-            {location.pathname !== "/planets"   && <Link to="/planets">Planets</Link>}
-            {location.pathname !== "/starships" && <Link to="/starships">Starships</Link>}
+        <div className={style.NavBarStyle}>
+            <div>
+                {location.pathname !== "/home"      && <Link to="/home">Home</Link>}
+                {location.pathname !== "/people"    && <Link to="/people">People</Link>}
+                {location.pathname !== "/planets"   && <Link to="/planets">Planets</Link>}
+                {location.pathname !== "/starships" && <Link to="/starships">Starships</Link>}
+            </div>
+            <h1>STAR WARS - API</h1>
+            <Link to="/">Salir</Link>
         </div>
     )
 }
