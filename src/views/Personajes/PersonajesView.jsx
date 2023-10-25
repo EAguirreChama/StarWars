f1b005c3fd30a8e23a62ce6a8d9dc4a12f4164db
EAguirre <aguirreernesto25@gmail.com>
2023-10-25 00:01:24 -0300

    Veamos si funciona

--- src/views/Personajes/PersonajesView.jsx
+++ src/views/Personajes/PersonajesView.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPersonajes } from "../../Redux/Personajes/getPersonajes";
+
 const Personajes = () => {
+    const dispatch = useDispatch()
+
+    const allPersonajes = useSelector(state => state.allPersonajes)
+
+    useEffect(() => {
+        !allPersonajes?.legth && dispatch(getPersonajes())
+    }, [dispatch, allPersonajes?.legth])
+    
     return (
         <div>
             <h1>Aqui van a estar todos los personajes con paginación y filtros</h1>
