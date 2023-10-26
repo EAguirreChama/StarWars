bb977535d82f0b6a70be22ac4df35902cc9c94dd
EAguirre <aguirreernesto25@gmail.com>
2023-10-26 15:21:17 -0300

    Landing Lista

--- StarWars Api/src/views/LandingView/LandingView.jsx
+++ StarWars Api/src/views/LandingView/LandingView.jsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+import LandindStyle from "./LandingView.module.css";
+
 const Landing = () => {
     return (
-        <div>
-            <h1>Aqui es Landing</h1>
+        <div className={LandindStyle.container}>
+            <h1>Welcome to Star Wars</h1>
+            <Link to="/dashboard">
+                <button className={LandindStyle.dashboardButton}>Ir al Dashboard</button>
+            </Link>
         </div>
-    )
+    );
 }
 
 export default Landing;
